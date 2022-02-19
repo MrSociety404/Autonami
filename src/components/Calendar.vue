1518e87a836eff38e6e42d6bcee1624311c6097b
ThomasBackers <thomasbackers@gmail.com>
2022-02-19 16:34:31 +0100

    calendar logic done - Thomas and Rousli

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,153 +1,137 @@
 <script setup>
-import Month from '../composables/Month'
-import { watchEffect } from 'vue'
-
-let currentMonth = new Date().getMonth() + 1
-let currentYear = new Date().getFullYear()
-let month = new Month(currentMonth, currentYear)
-
-const clickPrevious = () => {
-  currentMonth = currentMonth === 1 ? 12 : currentMonth - 1
-  currentYear = currentMonth === 12 ? currentYear - 1 : currentYear
-  month = new Month(currentMonth, currentYear)
-  console.log(currentMonth, currentYear)
-}
-
-const clickNext = () => {
-  currentMonth = currentMonth === 12 ? 1 : currentMonth + 1
-  currentYear = currentMonth === 1 ? currentYear + 1 : currentYear
-  month = new Month(currentMonth, currentYear)
-  console.log(currentMonth, currentYear)
-}
+defineProps({
+  month: Object
+})
 </script>
 
 <template>
   <div class="calendar">
-    <div class="calendar__header"></div>
-      <button class="previous" @click="clickPrevious">
+    <div class="calendar__header">
+      <button class="previous" @click="$emit('clickPrevious')">
         minus
       </button>
       
       <h1>{{ month.name }}</h1>
       
-      <button class="next" @click="clickNext">
+      <button class="next" @click="$emit('clickNext')">
         plus
       </button>
-  </div>
-
-  <div class="grid">
-    <div>
-      <p class="day">lun</p>
-      <p class="day">mar</p>
-      <p class="day">mer</p>
-      <p class="day">jeu</p>
-      <p class="day">ven</p>
-      <p class="day">sam</p>
-      <p class="day">dim</p>
     </div>
 
-    <div class="real-grid" v-if="month.days[0].weekday === 'lundi'">
-      <div
-        v-for="(day, i) of month.days"
-        :key="i"
-      >
-        {{ day.date }}
+    <div class="grid">
+      <div>
+        <p class="day">lun</p>
+        <p class="day">mar</p>
+        <p class="day">mer</p>
+        <p class="day">jeu</p>
+        <p class="day">ven</p>
+        <p class="day">sam</p>
+        <p class="day">dim</p>
       </div>
-    </div>
 
-    <div class="real-grid" v-else-if="month.days[0].weekday === 'mardi'">
-      <div></div>
-
-      <div
-        v-for="(day, i) of month.days"
-        :key="i"
-      >
-        {{ day.date }}
+      <div class="real-grid" v-if="month.days[0].weekday === 'lundi'">
+        <div
+          v-for="(day, i) of month.days"
+          :key="i"
+        >
+          {{ day.date }}
+        </div>
       </div>
-    </div>
 
-    <div class="real-grid" v-else-if="month.days[0].weekday === 'mercredi'">
-      <div></div>
+      <div class="real-grid" v-else-if="month.days[0].weekday === 'mardi'">
+        <div></div>
 
-      <div></div>
-
-      <div
-        v-for="(day, i) of month.days"
-        :key="i"
-      >
-        {{ day.date }}
+        <div
+          v-for="(day, i) of month.days"
+          :key="i"
+        >
+          {{ day.date }}
+        </div>
       </div>
-    </div>
 
-    <div class="real-grid" v-else-if="month.days[0].weekday === 'jeudi'">
-      <div></div>
+      <div class="real-grid" v-else-if="month.days[0].weekday === 'mercredi'">
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
-
-      <div
-        v-for="(day, i) of month.days"
-        :key="i"
-      >
-        {{ day.date }}
+        <div
+          v-for="(day, i) of month.days"
+          :key="i"
+        >
+          {{ day.date }}
+        </div>
       </div>
-    </div>
 
-    <div class="real-grid" v-else-if="month.days[0].weekday === 'vendredi'">
-      <div></div>
+      <div class="real-grid" v-else-if="month.days[0].weekday === 'jeudi'">
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
-
-      <div
-        v-for="(day, i) of month.days"
-        :key="i"
-      >
-        {{ day.date }}
+        <div
+          v-for="(day, i) of month.days"
+          :key="i"
+        >
+          {{ day.date }}
+        </div>
       </div>
-    </div>
 
-    <div class="real-grid" v-else-if="month.days[0].weekday === 'samedi'">
-      <div></div>
+      <div class="real-grid" v-else-if="month.days[0].weekday === 'vendredi'">
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
-
-      <div
-        v-for="(day, i) of month.days"
-        :key="i"
-      >
-        {{ day.date }}
+        <div
+          v-for="(day, i) of month.days"
+          :key="i"
+        >
+          {{ day.date }}
+        </div>
       </div>
-    </div>
 
-    <div class="real-grid" v-else-if="month.days[0].weekday === 'dimanche'">
-      <div></div>
+      <div class="real-grid" v-else-if="month.days[0].weekday === 'samedi'">
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
+        <div></div>
 
-      <div></div>
+        <div
+          v-for="(day, i) of month.days"
+          :key="i"
+        >
+          {{ day.date }}
+        </div>
+      </div>
 
-      <div
-        v-for="(day, i) of month.days"
-        :key="i"
-      >
-        {{ day.date }}
+      <div class="real-grid" v-else-if="month.days[0].weekday === 'dimanche'">
+        <div></div>
+
+        <div></div>
+
+        <div></div>
+
+        <div></div>
+
+        <div></div>
+
+        <div></div>
+
+        <div
+          v-for="(day, i) of month.days"
+          :key="i"
+        >
+          {{ day.date }}
+        </div>
       </div>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,34 @@
 <script setup>
-import Calendar from '../components/Calendar.vue';
+import Month from '../composables/Month'
+import Calendar from '../components/Calendar.vue'
+import { ref } from 'vue'
+
+let currentMonth = new Date().getMonth() + 1
+let currentYear = new Date().getFullYear()
+let month = ref(new Month(currentMonth, currentYear))
+
+const clickPrevious = () => {
+  currentMonth = currentMonth === 1 ? 12 : currentMonth - 1
+  currentYear = currentMonth === 12 ? currentYear - 1 : currentYear
+  month.value = new Month(currentMonth, currentYear)
+  console.log(currentMonth, currentYear)
+}
+
+const clickNext = () => {
+  currentMonth = currentMonth === 12 ? 1 : currentMonth + 1
+  currentYear = currentMonth === 1 ? currentYear + 1 : currentYear
+  month.value = new Month(currentMonth, currentYear)
+  console.log(currentMonth, currentYear)
+}
 </script>
 
 <template>
   <main>
-    <Calendar />
+    <Calendar
+      :month="month"
+      @clickPrevious="clickPrevious"
+      @clickNext="clickNext"
+    />
   </main>
 </template>
 
