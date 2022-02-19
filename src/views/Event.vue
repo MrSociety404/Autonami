1746542ef752339e57dc4142b039e93e72eeebb9
Fabrice <cstfabrice@gmail.com>
2022-02-19 18:57:23 +0100

    🦊 Add id to event

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,8 +1,11 @@
 <script setup>
-import { onMounted, ref } from 'vue';
 import Input from '../components/Input.vue';
 import Button from '../components/Button.vue';
+
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
+
+import { v4 as uuid } from 'uuid'
 
 const router = useRouter()
 
@@ -15,10 +18,11 @@ const today = new Date().toLocaleString("fr-FR", {
 const user = ref(null)
 
 const formdata = ref({
+  id: uuid(),
   name: "",
   date: "",
   heure: "",
-  help: 0,
+  help: "0",
   location: "",
 })
 
@@ -43,8 +47,46 @@ const addEvent = () => {
     <form @submit.prevent class="px-6 py-4 z-10">
       <Input label="Nom de l'activité" v-model="formdata.name" />
       <Input label="Jour" type="date" :placeholder="today" v-model="formdata.date" />
-      <Input label="Heure" type="time" v-model="formdata.time" />
+      <Input label="Heure" type="time" v-model="formdata.heure" />
       <Input label="Lieu" v-model="formdata.location" />
+      <div>
+        <h3 class="font-semibold text-xl my-5">Un coup de main ?</h3>
+        <div class="radio">
+          <input
+            type="radio"
+            class="radio__input"
+            id="1"
+            name="help"
+            value="1"
+            v-model="formdata.help"
+          />
+          <label for="1" class="radio__label">Accompagné d'un bénévole</label>
+        </div>
+
+        <div class="radio">
+          <input
+            type="radio"
+            class="radio__input"
+            id="2"
+            name="help"
+            value="2"
+            v-model="formdata.help"
+          />
+          <label for="2" class="radio__label">Accompagné d'un proche</label>
+        </div>
+
+        <div class="radio">
+          <input
+            type="radio"
+            class="radio__input"
+            id="0"
+            name="help"
+            value="0"
+            v-model="formdata.help"
+          />
+          <label for="0" class="radio__label">Non merci</label>
+        </div>
+      </div>
       <Button class="bg-secondary w-full" @click="addEvent()">Ajouter</Button>
       <Button class="bg-primary text-white w-full" @click="goBack()">Annuler</Button>
     </form>
@@ -52,4 +94,22 @@ const addEvent = () => {
 </template>
 
 <style lang="scss">
+.radio {
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+  margin-block: 1rem;
+  font-size: 1.2rem;
+  &__input {
+    width: 25px;
+    height: 25px;
+    background-color: #e7e5e4;
+    border: 5px solid #dbd5d2;
+    border-radius: 50%;
+    &:checked {
+      border-color: #ff9a55;
+      background-color: #ffdfd0;
+    }
+  }
+}
 </style>
